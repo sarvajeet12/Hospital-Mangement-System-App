46e79b4ea55df1081b2beec4bcf4c38f9e89e5bd
Sarvajeet Lal Shah <132431162+sarvajeet12@users.noreply.github.com>
2025-09-11 00:05:53 +0530

    Update upload.js

--- Server/utils/upload.js
+++ Server/utils/upload.js
@@ -3,7 +3,7 @@ const Cloudinary = require('cloudinary').v2
 
 const uploadToCloudinary = async (file, folder, height, quality) => {
 
-    const options = { folde };
+    const options = { folder };
 
     if (height) {
         options.height = height;
