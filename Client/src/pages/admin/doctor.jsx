bd8ed1c4632409d1b26c2a43912ef9b7b8c56be6
Sarvajeet Lal Shah <132431162+sarvajeet12@users.noreply.github.com>
2025-09-22 21:25:06 +0530

    Update doctor.jsx

--- Client/src/pages/admin/doctor.jsx
+++ Client/src/pages/admin/doctor.jsx
@@ -1,4 +1,4 @@
-import React from "reac";
+import React from "react";
 import DoctorList from "../../components/core/admin-dashboad-page/doctor-list";
 import Style from "../../components/core/admin-dashboad-page/admin.module.css";
 
